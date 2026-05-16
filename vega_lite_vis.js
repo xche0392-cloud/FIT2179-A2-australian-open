433ead5fd74c2f6c8aeb7d4c9a932edb0579a0ee
xingyu <cxxx@MacBook-Pro-5.local>
2026-05-17 03:35:16 +1000

    add prize chart and other charts

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,2 +1,17 @@
 vegaEmbed("#attendance_chart", "js/attendance_chart.vg.json").then(function(result) {
 }).catch(console.error);
+
+vegaEmbed("#womens_champions_chart", "js/womens_champions_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#mens_champions_chart", "js/mens_champions_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#top_nations_chart", "js/top_nations_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#aus_results_chart", "js/aus_results_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#prize_chart", "js/prize_chart.vg.json").then(function(result) {
+}).catch(console.error);
