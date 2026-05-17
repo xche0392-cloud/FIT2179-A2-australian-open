81a2d3a499bd1277427e00c4f5db9883b0d5ae8f
xingyu <cxxx@MacBook-Pro-5.local>
2026-05-17 18:37:44 +1000

    complete all charts

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -15,3 +15,18 @@ vegaEmbed("#aus_results_chart", "js/aus_results_chart.vg.json").then(function(re
 
 vegaEmbed("#prize_chart", "js/prize_chart.vg.json").then(function(result) {
 }).catch(console.error);
+
+vegaEmbed("#titles_by_country_chart", "js/titles_by_country_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#legend_heatmap", "js/legend_heatmap.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#age_winrate_chart", "js/age_winrate_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#world_map", "js/world_map.vg.json").then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#aus_states_map", "js/aus_states_map.vg.json").then(function(result) {
+}).catch(console.error);
