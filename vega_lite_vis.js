68d21c0e17dd3bb0f97a60606182fddf2aea8a54
xingyu <cxxx@MacBook-Pro-5.local>
2026-05-31 13:03:05 +1000

    feat: add year filter controls to attendance and prize money charts

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,9 @@
 vegaEmbed("#attendance_chart", "js/attendance_chart.vg.json").then(function(result) {
 }).catch(console.error);
 
+vegaEmbed("#prize_chart", "js/prize_chart.vg.json").then(function(result) {
+}).catch(console.error);
+
 vegaEmbed("#womens_champions_chart", "js/womens_champions_chart.vg.json").then(function(result) {
 }).catch(console.error);
 
@@ -11,9 +14,6 @@ vegaEmbed("#top_nations_chart", "js/top_nations_chart.vg.json").then(function(re
 }).catch(console.error);
 
 vegaEmbed("#aus_results_chart", "js/aus_results_chart.vg.json").then(function(result) {
-}).catch(console.error);
-
-vegaEmbed("#prize_chart", "js/prize_chart.vg.json").then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#titles_by_country_chart", "js/titles_by_country_chart.vg.json").then(function(result) {
